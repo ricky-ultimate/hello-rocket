ef175b18628256de5aae6679a51f6f81cb6a6eca
Ricky Ultimate <rickyogigi@gmail.com>
2025-04-01 17:45:25 +0100

    [feat] Playing around with rocket code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 #[macro_use] extern crate rocket;
 
 
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/earth")]
+fn earth() -> &'static str {
+    "Hello, earth!"
+}
+
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "This is the start!"
 }
 
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/hello", routes![index, world, earth])
 }
