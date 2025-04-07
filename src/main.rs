6d676d7dc0030327e8d729228fa902f5bd60e570
Ricky Ultimate <rickyogigi@gmail.com>
2025-04-07 10:34:56 +0100

    feat: create login functionality that extends FromRequest

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,73 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
+use rocket::http::Cookie;
+use rocket::http::Status;
+use rocket::http::CookieJar;
+use rocket::request::{FromRequest, Outcome, Request};
+use rocket::response::Redirect;
 
-#[get("/world")]
-fn world() -> &'static str {
-    "Hello, world!"
+#[derive(Debug)]
+struct User {
+    user_id: String,
+    username: String,
 }
 
-#[get("/earth")]
-fn earth() -> &'static str {
-    "Hello, earth!"
+#[derive(Debug)]
+enum AuthError {
+    MissingCookie,
+    InvalidCookie,
 }
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for User {
+    type Error = AuthError;
+
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let cookies = request.cookies();
+        let user_cookie = cookies.get("user_id");
+
+        match user_cookie {
+            Some(cookie) => {
+                if cookie.value() == "abc-xyz-123" {
+                    Outcome::Success(User {
+                        user_id: "abc-xyz-123".to_string(),
+                        username: "rando".to_string(),
+                    })
+                } else {
+                    Outcome::Error((Status::Unauthorized, AuthError::InvalidCookie))
+                }
+            }
+
+            None => Outcome::Error((Status::Unauthorized, AuthError::MissingCookie))
+        }
+    }
+}
+
 
 #[get("/")]
-fn index() -> &'static str {
-    "This is the start!"
+fn index(user: User) -> String {
+    format!("Hello, {}! You're logged in with ID: {}", user.username, user.user_id)
 }
 
-#[get("/hello/<name>/<age>")]
-fn hello(name: &str, age: u8) -> String {
-    format!("Hello, {}. You claim to be {}, but is that really your age 🤔", name, age)
+#[get("/", rank = 2)]
+fn index_guest() -> &'static str {
+    "Hello, guest! Please log in"
+}
+
+#[get("/login")]
+fn login(cookies: &CookieJar<'_>) -> Redirect {
+    cookies.add(Cookie::new("user_id", "abc-xyz-123"));
+    Redirect::to("/")
+}
+
+#[get("/logout")]
+fn logout(cookies: &CookieJar<'_>) -> Redirect {
+    cookies.remove("user_id");
+    Redirect::to("/")
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![hello])
-        .mount("/hello", routes![index, world, earth])
+    rocket::build().mount("/", routes![index, index_guest, login, logout])
 }
